846f660204d343925140c34fd22b2ba05e3cb102
Caeley Collins <cbaneydev@gmail.com>
2022-07-08 19:42:12 -0500

    finished project

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return "This one is on me."
+  } else if (feet > 2000 && feet < 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+} 
+
+console.log(scuberGreetingForFeet(2600))
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Okay, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+console.log(ternaryCheckCity("AR"))
 
 function switchOnCharmFromTip(){
   // Write your code here!
